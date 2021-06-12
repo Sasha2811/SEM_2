835d039cd4b013375d1dd3c9b377843815e6a93f
SashaMoskalets <59283185+Sasha2811@users.noreply.github.com>
2021-06-12 13:37:12 +0300

    Add files via upload

---
+++ LAB_4/Lab_4_Sem_2_1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_4_Sem_2_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            comboBox.Items.Add(textBox.Text);
+            textBox.Text = "";
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            comboBox.Items.Clear();
+            textBox.Text = "";
+        }
+    }
+}
